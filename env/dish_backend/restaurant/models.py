88beec54094d37da15e832298dd444cb2396edbd
Julian Chairez <julian.chairez805@gmail.com>
2024-10-31 10:30:03 -0500

    Changes to table to allow null values

--- env/dish_backend/restaurant/models.py
+++ env/dish_backend/restaurant/models.py
@@ -5,16 +5,15 @@ class Restaurant(models.Model):
     external_id = models.CharField(max_length=50, unique=True, blank=True, null=True)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=15)
+    phone_number = models.CharField(max_length=15, null=True)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
-    county = models.CharField(max_length=100)
     lat = models.FloatField()
     lng = models.FloatField()
-    image_url = models.URLField()
-    menu_url = models.URLField()
-    category = models.CharField(max_length=100)
-    review_count = models.DecimalField(max_digits=3, decimal_places=2)
+    image_url = models.URLField(null=True)
+    menu_url = models.URLField(null=True)
+    category = models.JSONField(default=list, null=True) 
+    review_count = models.DecimalField(max_digits=3, decimal_places=2, null=True)
 
     def __str__(self):
         return self.name
